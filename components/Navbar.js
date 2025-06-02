b075cd273c52a5731b67b671ac70d817c07c46a7
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2025-06-03 00:27:05 +0530

    Navbar created and Protected route implemented

---
+++ components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+
+function Navbar() {
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center px-30">
+        <Link href="/" className="text-xl font-bold">
+          My App
+        </Link>
+        <div className="flex gap-10">
+        <ul>
+          <li className="flex space-x-2">
+            <Link href="/" className="hover:text-gray-200">
+              Home
+            </Link>
+            <Link href="/about" className="hover:text-gray-200">
+              About
+            </Link>
+            <Link href="/contact" className="hover:text-gray-200">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <UserButton/>
+        </div>
+      </div>
+    </nav>
+    
+  );
+}
+
+export default Navbar;
